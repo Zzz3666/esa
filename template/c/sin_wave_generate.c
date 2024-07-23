8dfd5c9eb5b2c7d799c56cfb3671cbe1f6a8a163
aworkshop <zhouyan_90@live.cn>
2024-07-23 08:33:48 +0800

    fix(template): adjust sin wave generation formula in C code
    Correct the calculation in sin_wave_generate function to use division
    instead of multiplication by 10, ensuring accurate sin wave data
    generation based on the updated formula.

--- template/c/sin_wave_generate.c
+++ template/c/sin_wave_generate.c
@@ -4,6 +4,6 @@ uint32_t sin_wave_data[SIN_WAVE_LENGTH] = "replace_sin_wave_data";
 
 uint32_t sin_wave_generate(uint32_t time_tick, uint32_t frequency)
 {
-    
-    return sin_wave_data[(time_tick * frequency) % SIN_WAVE_LENGTH];
+
+    return sin_wave_data[(time_tick / 10 * frequency) % SIN_WAVE_LENGTH];
 }
